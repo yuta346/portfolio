38b3b5fe149bdf017609aa6fe454fce58c0a8a70
Yuta Hagiwara <hagiwara.y@outlook.com>
2021-08-27 11:06:03 -0400

    fix padding

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,11 +23,11 @@ const Navbar = () => {
                 </a>
                 <div className={isActive ? "nav-items-toggle" : "nav-items"}>
                     <ul id="nav">
-                        <li><a href="#about"><b>Home</b></a></li>
-                        <li><a href="#experiences">Experience</a></li>
-                        <li><a href="#projects">Project</a></li>
+                        <li><a href="#about" onClick={toggleHandler}><b>Home</b></a></li>
+                        <li><a href="#experiences" onClick={toggleHandler}>Experience</a></li>
+                        <li><a href="#projects" onClick={toggleHandler}>Project</a></li>
                         <li><a href = {Pdf} target = "_blank">Resume</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#contact" onClick={toggleHandler}>Contact</a></li>
                     </ul>
                 </div>
             </div>)      
